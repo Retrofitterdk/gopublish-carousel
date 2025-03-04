61d46f8508af8d2454b9071a954b5651a67488fa
Retrofitterdk <sbn@retrofitter.dk>
2025-03-04 22:08:29 +0100

    Creating an experiment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bd3d4f6ef50ea0372494');
+=======
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'aa74d240243c4d5acac3');
+>>>>>>> Stashed changes
 =======
 <?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'aa74d240243c4d5acac3');
 >>>>>>> Stashed changes
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,5 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '36b9f2a71a090e1789db', 'type' => 'module');
+=======
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '0c3753c480ac25b2b8c6', 'type' => 'module');
+>>>>>>> Stashed changes
 =======
 <?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '0c3753c480ac25b2b8c6', 'type' => 'module');
 >>>>>>> Stashed changes
