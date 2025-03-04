2bffdb29d5e0c641042d6fff42e7ca8a4f672800
Retrofitterdk <sbn@retrofitter.dk>
2025-03-04 22:08:12 +0100

    Creating an experiment

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bd3d4f6ef50ea0372494');
+=======
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'aa74d240243c4d5acac3');
+>>>>>>> Stashed changes
