532e535e7abbae5cf83cb2fbacd61980e9da2d1e
RP90 <rplambech90@gmail.com>
2025-03-07 13:04:27 +0100

    Fix: increase target area of inner-block

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '56d64c59bcdf5de14d22');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'b064dd915192c13d4c3b');
