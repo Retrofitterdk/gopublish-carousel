1c427c1a1ce2e1b062a6fb744022f3cfc0af6728
RP90 <rplambech90@gmail.com>
2025-03-07 12:30:59 +0100

    Ads support for alignwide and alignfull, and removes client specific alignment.
    Refs #1

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '82d67f6db2c5dc9a9592');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '56d64c59bcdf5de14d22');
--- build/carousel/index.js
+++ build/carousel/index.js
@@ -241,7 +241,7 @@ module.exports = window["wp"]["components"];
   \*********************************/
 /***/ ((module) => {
 
-module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"squareonesoftware/demo-carousel","version":"0.1.0","title":"Carousel","category":"widgets","icon":"media-interactive","description":"A carousel block using the Interactivity API.","example":{},"supports":{"interactivity":true},"attributes":{"columns":{"type":"number","default":3},"autoplay":{"type":"boolean","default":false},"scroll":{"type":"number","default":1}},"textdomain":"demo-carousel","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScriptModule":"file:./view.js"}');
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"squareonesoftware/demo-carousel","version":"0.1.0","title":"Carousel","category":"widgets","icon":"media-interactive","description":"A carousel block using the Interactivity API.","example":{},"supports":{"align":["wide","full"],"interactivity":true},"attributes":{"columns":{"type":"number","default":3},"autoplay":{"type":"boolean","default":false},"scroll":{"type":"number","default":1}},"textdomain":"demo-carousel","editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","render":"file:./render.php","viewScriptModule":"file:./view.js"}');
 
 /***/ })
 
