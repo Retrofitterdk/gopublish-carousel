1bca8c38e6defc4f904a871455ef28d389256ac8
Retrofitterdk <sbn@retrofitter.dk>
2025-09-28 23:48:46 +0200

    Update index.asset.php

--- build/carousel-slide/index.asset.php
+++ build/carousel-slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'c580560861f62246433a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '148fad8089c2208e0098');
