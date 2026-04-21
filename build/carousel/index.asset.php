684f5f81cc91fc3179bd404e25ff7add3fb7064f
Retrofitterdk <sbn@retrofitter.dk>
2026-04-21 15:12:38 +0200

    Buiild files

--- build/carousel/index.asset.php
+++ build/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'beedb0bb56a934337545');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fb5cc6ff6b7df2aba5e9');
